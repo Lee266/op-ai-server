b9d1faedc0aec3c6d65701af66807e66b41094fa
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-29 00:10:55 +0900

    refactor[routes]: jwt認証が適応されたapiを追加、url mapの作成

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/Lee266/op-ai-server/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,18 +11,69 @@ func Router() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "dashboard"})
+		// 使えるAPIのURL一覧を定義
+		apiUrls := []string{
+			"/api/public",
+			"/api/private",
+			"/api/private-scoped",
+		}
+
+		c.JSON(http.StatusOK, gin.H{"api_urls": apiUrls})
 	})
 
-	auth := r.Group("/auth")
+	// 保護されていないエンドポイント
+	publicGroup := r.Group("/api/public")
 	{
-		auth.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{"message": "auth"})
+		publicGroup.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "Hello from a public endpoint! You don't need to be authenticated to see this."})
 		})
 	}
 
+	// トークンの検証が必要なエンドポイント
+	privateGroup := r.Group("/api/private")
+	privateGroup.Use(middlewares.EnsureValidToken())
+	{
+		privateGroup.GET("/", func(c *gin.Context) {
+			// CORS Headers
+			c.Header("Access-Control-Allow-Credentials", "true")
+			c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+			c.Header("Access-Control-Allow-Headers", "Authorization")
+
+			c.JSON(http.StatusOK, gin.H{"message": "Hello from a private endpoint! You need to be authenticated to see this."})
+		})
+	}
+
+	// トークンの検証とスコープのチェックが必要なエンドポイント
+	privateScopedGroup := r.Group("/api/private-scoped")
+	privateScopedGroup.Use(middlewares.EnsureValidToken())
+	{
+		privateScopedGroup.GET("/", func(c *gin.Context) {
+			// CORS Headers
+			c.Header("Access-Control-Allow-Credentials", "true")
+			c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+			c.Header("Access-Control-Allow-Headers", "Authorization")
+
+			// トークンが必要なスコープを持っているか確認
+			tokenClaims, exists := c.Get("claims")
+			if !exists {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Token claims not found"})
+				return
+			}
+
+			claims, ok := tokenClaims.(*middlewares.CustomClaims)
+			if !ok || !claims.HasScope("read:messages") {
+				c.JSON(http.StatusForbidden, gin.H{"message": "Insufficient scope."})
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{"message": "Hello from a private-scoped endpoint! You need to be authenticated and have the 'read:messages' scope to see this."})
+		})
+	}
+
+	// 存在しないルートの処理
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Not Found"})
 	})
+
 	return r
 }
--- router/router_test.go
+++ router/router_test.go
@@ -1,11 +1,12 @@
 package router_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/Lee266/op-ai-server/router" // パスを実際のプロジェクトの構造に合わせて変更
+	"github.com/Lee266/op-ai-server/router"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,7 +19,20 @@ func TestRouter(t *testing.T) {
 	r.ServeHTTP(resp, req)
 
 	assert.Equal(t, http.StatusOK, resp.Code)
-	assert.Contains(t, resp.Body.String(), "dashboard")
+
+	var responseBody map[string][]string
+	err := json.Unmarshal(resp.Body.Bytes(), &responseBody)
+	assert.NoError(t, err)
+
+	assert.ElementsMatch(t, []string{"/api/public", "/api/private", "/api/private-scoped"}, responseBody["api_urls"])
+
+	// "/api/public/"へのGETリクエストのテスト
+	req, _ = http.NewRequest("GET", "/api/public/", nil)
+	resp = httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+	assert.Contains(t, resp.Body.String(), "Hello from a public endpoint")
 
 	// 存在しないパスへのリクエストのテスト
 	req, _ = http.NewRequest("GET", "/nonexistent", nil)
